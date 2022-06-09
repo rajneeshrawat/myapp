471d519b862ae5b06a3baaa99ce13173522f0d47
rajneesh <rawatrajneesh687@gmail.com>
2022-06-09 11:53:23 +0530

    get post

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var rajneeshRouter = require('./routes/rajneesh');
-var myselfRouter = require('./routes/myself');
+var studentRouter = require('./routes/student');
+
 var app = express();
 
 // view engine setup
+// app.use(express.static('views',{extensions:['html']}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.set('view engine', 'hbs');
+// app.engine('html',require('hogan-express'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/rajneesh',rajneeshRouter)
-app.use('/myself',myselfRouter)
+app.use('/student', studentRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
